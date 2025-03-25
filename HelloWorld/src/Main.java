fc41107332fc1427d57991c6d9427bdcf2a10742
woohyun-park <qkrdngus1227@gmail.com>
2025-03-25 11:44:46 +0900

    2025.03.25

---
+++ HelloWorld/src/Main.java
@@ -0,0 +1,11 @@
+public class Main {
+    public static void main(String[] args) {
+        double half = 10.0;
+        final double PI = 3.14;
+        double area = half * half * PI;
+
+        System.out.printf("반지름이 %f인 원의 면적은 %f입니다\n",half ,area);
+        System.out.println("반지름이" + half + "인 원의 면적은" + area + "이다");
+
+    }
+}
